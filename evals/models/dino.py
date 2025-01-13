cb3c49859717dc56d8b2a9b5bfe927cb3f3682d1
duc12111 <nguyentienduc5@gmail.com>
2025-01-13 01:59:25 +0100

    add dino_s16 and vjepa_h16

--- evals/models/dino.py
+++ evals/models/dino.py
@@ -14,6 +14,7 @@ class DINO(torch.nn.Module):
     ):
         super().__init__()
         feat_dims = {
+            "vits16": 384,
             "vitb8": 768,
             "vitb16": 768,
             "vitb14": 768,
